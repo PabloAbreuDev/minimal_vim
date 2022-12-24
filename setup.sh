032c5f3afdc354267355096f4c0099cefd7d0eab
PabloAbreuDev <pabloabreudev@gmail.com>
2022-12-24 00:54:09 -0300

    first

---
+++ setup.sh
@@ -0,0 +1,24 @@
+##!/bin/bash
+# Installing Neovim
+mkdir ~/.config/nvim/
+wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+mv ./nvim.appimage ~/.config/nvim/
+chmod u+x ~/.config/nvim/nvim.appimage
+
+echo 'alias nvim="~/.config/nvim/nvim.appimage"' >> ~/.zshrc
+echo 'alias vim="~/.config/nvim/nvim.appimage"' >> ~/.zshrc
+echo 'alias vi="~/.config/nvim/nvim.appimage"' >> ~/.zshrc
+
+
+# Vim plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Save neovim configs
+cp init.vim ~/.config/nvim/
+
+# Install plugins
+~/.config/nvim/nvim.appimage --headless +PlugInstall +qall
+~/.config/nvim/nvim.appimage --headless -c 'CocInstall coc-json coc-tsserver' -c 'qall'
+
+exec zsh
